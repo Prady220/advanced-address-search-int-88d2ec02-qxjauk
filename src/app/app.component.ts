05976edb19c641d127a5add8c70d5176e9419240
Prady220 <pradeep.ece.gitam@gmail.com>
2021-05-22 03:04:08 -0500

    Added few comments to subject vs observable scenario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -140,8 +140,7 @@ export class AppComponent implements OnInit {
   //Example for the difference between observable and subject that we discussed in the interview
   subVsObs() {
     let observable = Observable.create(obs => obs.next(Math.random()));
-    //If of() is used instead of create() then the behavior is similar to Subject
-    //Which was the issue I could not figure out in the interview
+    //If of() is used instead of create() then the behavior is similar to Subject (Interview Scenario)
     observable.subscribe(v => this.obsSub1 = v);
     observable.subscribe(v => this.obsSub2 = v);
 
